0813d5eb407711cd1c804fac881ec35eed42f081
kathiresanh <hkathiresan@gmail.com>
2021-12-19 22:58:51 +0530

    react todo list

---
+++ src/List.js
@@ -0,0 +1,17 @@
+import react from "react";
+
+export default function List(props) {
+    return <div>
+        <ul>
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">{props.indexes} {props.datas}</div>
+                  
+                </div>
+                <button onClick={()=>{props.delEvent(props.indexes)}} className="badge bg-danger rounded-pill">X</button>
+            </li>
+
+
+        </ul>
+    </div>
+}
