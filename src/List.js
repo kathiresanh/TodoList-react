ff883efd71b943ddf0da3edc66f1924fd13f878d
kathiresanh <hkathiresan@gmail.com>
2021-12-20 07:14:54 +0530

    mytodolist first

--- src/List.js
+++ src/List.js
@@ -1,14 +1,14 @@
 import react from "react";
 
-export default function List(props) {
+export default function List(prop) {
     return <div>
         <ul>
             <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
-                    <div className="fw-bold">{props.indexes+1} {props.datas}</div>
+                    <div className="fw-bold">{prop.indexes+1} {prop.datas}</div>
                   
                 </div>
-                <button onClick={()=>{props.delEvent(props.indexes)}} className="badge bg-danger rounded-pill">X</button>
+                <button onClick={()=>{prop.delEvent(prop.indexes)}} className="badge bg-danger rounded-pill">X</button>
             </li>
 
 
